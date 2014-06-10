2cfe38a65531a5fa0e510db070ccd8b2d10f6512
Keiichiro Ono <keiono@gmail.com>
2014-06-09 18:57:09 -0700

    Very simple test added.

--- spec/cy_rest_spec.js
+++ spec/cy_rest_spec.js
@@ -18,6 +18,11 @@ var BASE_URL = "http://localhost:1234/v1/";
 var yeastNetworkJson = require('./yeast1.json');
 console.log(yeastNetworkJson.data);
 
+
+var firstNetworkID = null;
+
+// TODO: initialize Cytoscape.
+
 frisby.create('Get number of networks in current session. (Assume this is a new session)')
     .get(BASE_URL + 'networks/count')
     .expectStatus(200)
@@ -31,12 +36,21 @@ frisby.create('Get number of networks in current session. (Assume this is a new 
     .toss();
 
 frisby.create('POST a network (Total # of network is 1.')
-    .post('http://localhost:1234/v1/networks', yeastNetworkJson, {
+    .post(BASE_URL + 'networks', yeastNetworkJson, {
         'json': true,
         'headers': {
             'Content-Type': 'application/json',
-            'user-agent': 'node.js'
+            'user-agent': 'frisby.js testing framework'
         }
     })
     .expectStatus(200)
+    .expectJSONTypes({
+        networkSUID: Number
+    })
+    .afterJSON(function(json) {
+        firstNetworkID = json.networkSUID;
+        console.log("New Network SUID = " + firstNetworkID);
+    })
     .toss();
+
+
