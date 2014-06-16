4edcfffe7a85e4d3e79a04ac09f42e5906cd3e46
Keiichiro Ono <keiono@gmail.com>
2014-06-16 12:49:20 -0700

    Minor update only.

--- spec/cy_rest_spec.js
+++ spec/cy_rest_spec.js
@@ -16,13 +16,11 @@ var BASE_URL = "http://localhost:1234/v1/";
 /////////////// Actual tests /////////////////
 
 var yeastNetworkJson = require('./yeast1.json');
-console.log(yeastNetworkJson.data);
-
-
 var firstNetworkID = null;
 
 // TODO: initialize Cytoscape.
 
+console.log("Test start...");
 frisby.create('Get number of networks in current session. (Assume this is a new session)')
     .get(BASE_URL + 'networks/count')
     .expectStatus(200)
@@ -35,6 +33,22 @@ frisby.create('Get number of networks in current session. (Assume this is a new 
     })
     .toss();
 
+
+console.log("Test count");
+frisby.create('Get number of networks in current session. (Assume this is a new session)')
+    .get(BASE_URL + 'networks/count')
+    .expectStatus(200)
+    .expectHeaderContains('Content-Type', 'application/json')
+    .expectJSON({
+        networkCount: 0
+    })
+    .expectJSONTypes({
+        networkCount: Number
+    })
+    .toss();
+
+
+console.log("Test POST");
 frisby.create('POST a network (Total # of network is 1.')
     .post(BASE_URL + 'networks', yeastNetworkJson, {
         'json': true,
@@ -50,7 +64,14 @@ frisby.create('POST a network (Total # of network is 1.')
     .afterJSON(function(json) {
         firstNetworkID = json.networkSUID;
         console.log("New Network SUID = " + firstNetworkID);
+        frisby.create('Get the new network data')
+            .get(BASE_URL + 'networks/' + firstNetworkID)
+            .expectStatus(200)
+            .expectHeaderContains('Content-Type', 'application/json')
+            .afterJSON(function(json) {
+                var data = json.data.SUID;
+                console.log("Extracted SUID = " + data);
+            })
+            .toss();
     })
     .toss();
-
-
