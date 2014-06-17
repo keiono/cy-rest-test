e480912258b5a32ba594fdac90476604e64328a0
Keiichiro Ono <keiono@gmail.com>
2014-06-16 17:44:47 -0700

    Some new tests have been added.

--- spec/cy_rest_spec.js
+++ spec/cy_rest_spec.js
@@ -7,71 +7,174 @@ var fs = require('fs');
 /**
  * Comprehensive test suite for Cytoscape REST API
  */
-
 var BASE_URL = "http://localhost:1234/v1/";
-
-/////////////// Helper Functions ////////////////
-
 
 /////////////// Actual tests /////////////////
 
 var yeastNetworkJson = require('./yeast1.json');
-var firstNetworkID = null;
+var emptyNetwork = {
+    data: {
+        name: 'empty network'
+    },
+    elements: {
+        nodes:[],
+        edges:[]
+    }
+};
 
 // TODO: initialize Cytoscape.
+describe("Suite 1: Network Model API tests", function() {
 
-console.log("Test start...");
-frisby.create('Get number of networks in current session. (Assume this is a new session)')
-    .get(BASE_URL + 'networks/count')
-    .expectStatus(200)
-    .expectHeaderContains('Content-Type', 'application/json')
-    .expectJSON({
-        networkCount: 0
-    })
-    .expectJSONTypes({
-        networkCount: Number
-    })
-    .toss();
+    console.log("############### Cytoscpe REST API Test Suite #####################");
+    it("Cleanup.  Delete all networks.", function(done) {
+        deleteAll();
+        done();
+        count(0);
+    });
+
+    it("Create an empty network.", function(done) {
+        console.log("Creating empty network...");
+        createEmptyNetwork();
+        done();
+        count(1);
+        getNetwork('name', 'empty network');
+    });
+
+    it("Post first network.", function(done) {
+        console.log("#2 Posting");
+        post();
+        done();
+    });
+
+    it("Post first network 2.", function(done) {
+        post();
+        done();
+    });
+
+    it("Check number of network.", function() {
+        console.log("#3 Checking network count");
+        count(3);
+    });
+
+    it("contains spec with an expectation", function(done) {
+        post2();
+        console.log("####### PASSED2.");
+        done();
+    });
+    it("Get all networks.", function(done) {
+        getAllNetworks();
+        done();
+    });
+
+    it("test4", function() {
+        count(4);
+
+    });
+});
 
 
-console.log("Test count");
-frisby.create('Get number of networks in current session. (Assume this is a new session)')
-    .get(BASE_URL + 'networks/count')
-    .expectStatus(200)
-    .expectHeaderContains('Content-Type', 'application/json')
-    .expectJSON({
-        networkCount: 0
-    })
-    .expectJSONTypes({
-        networkCount: Number
-    })
-    .toss();
+function count(numberOfNet) {
+    console.log("Counting...");
+    frisby.create('Get number of networks in current session. (Assume this is a new session)')
+        .get(BASE_URL + 'networks/count')
+        .expectStatus(200)
+        .expectHeaderContains('Content-Type', 'application/json')
+        .expectJSON({
+            networkCount: numberOfNet
+        })
+        .expectJSONTypes({
+            networkCount: Number
+        })
+        .toss();
+}
 
+function getNetwork(column, query) {
+    frisby.create('Get a network by query: ' + query)
+        .get(BASE_URL + 'networks?column=' + column + '&query=' + query)
+        .expectStatus(200)
+        .expectHeaderContains('Content-Type', 'application/json')
+        .inspectJSON()
+        .afterJSON(function(json) {
+            expect(json).toBeDefined();
+            var len = json.length;
+            expect(len).toBe(1);
 
-console.log("Test POST");
-frisby.create('POST a network (Total # of network is 1.')
-    .post(BASE_URL + 'networks', yeastNetworkJson, {
-        'json': true,
-        'headers': {
-            'Content-Type': 'application/json',
-            'user-agent': 'frisby.js testing framework'
-        }
-    })
-    .expectStatus(200)
-    .expectJSONTypes({
-        networkSUID: Number
-    })
-    .afterJSON(function(json) {
-        firstNetworkID = json.networkSUID;
-        console.log("New Network SUID = " + firstNetworkID);
-        frisby.create('Get the new network data')
-            .get(BASE_URL + 'networks/' + firstNetworkID)
-            .expectStatus(200)
-            .expectHeaderContains('Content-Type', 'application/json')
-            .afterJSON(function(json) {
-                var data = json.data.SUID;
-                console.log("Extracted SUID = " + data);
-            })
-            .toss();
-    })
-    .toss();
+            var network = json[0];
+            expect(network).toBeDefined();
+            var networkName = network.data.name;
+            expect(networkName).toBe('empty network');
+        })
+        .toss();
+
+}
+
+function getAllNetworks() {
+    frisby.create('Get number of networks in current session. (Assume this is a new session)')
+        .get(BASE_URL + 'networks')
+        .expectStatus(200)
+        .expectHeaderContains('Content-Type', 'application/json')
+        .toss();
+}
+
+function deleteAll() {
+    console.log("Deleting...");
+    frisby.create('Get number of networks in current session. (Assume this is a new session)')
+        .delete(BASE_URL + 'networks')
+        .expectStatus(204)
+        .toss();
+}
+
+function createEmptyNetwork() {
+    frisby.create('POST an empty network.')
+        .post(BASE_URL + 'networks',emptyNetwork, {
+            'json': true,
+            'headers': {
+                'Content-Type': 'application/json',
+                'user-agent': 'frisby.js testing framework'
+            }
+        })
+        .expectStatus(200)
+        .expectJSONTypes({
+            networkSUID: Number
+        })
+        .inspectJSON()
+        .toss();
+}
+
+function post() {
+
+    console.log("POSTing");
+    frisby.create('POST a new network.')
+        .post(BASE_URL + 'networks', yeastNetworkJson, {
+            'json': true,
+            'headers': {
+                'Content-Type': 'application/json',
+                'user-agent': 'frisby.js testing framework'
+            }
+        })
+        .expectStatus(200)
+        .expectJSONTypes({
+            networkSUID: Number
+        })
+        .inspectJSON()
+        .afterJSON(function(json) {
+            var suid = json.networkSUID;
+            expect(suid).toBeDefined();
+            console.log('SUID: ' + suid);
+        })
+        .toss();
+}
+
+function post2() {
+    console.log("Posting new network 2");
+    frisby.create('POST a network (Total # of network is 1.')
+        .post(BASE_URL + 'networks', yeastNetworkJson, {
+            'json': true,
+            'headers': {
+                'Content-Type': 'application/json',
+                'user-agent': 'frisby.js testing framework'
+            }
+        })
+        .expectStatus(200)
+        .toss();
+}
